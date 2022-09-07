3571fef9ac3b4466f8db6352f0901ad21a45623a
ignatioferreira <ignatio.ferreira@gmail.com>
2022-09-07 16:17:00 +0200

    progress

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,8 +19,7 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2017 Tim Baker</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>Made using <a href="https://github.com/tbakerx/react-resume-template">Tim Baker's</a> github project as a template</li>
            </ul>
 
         </div>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,10 +4,8 @@ class Header extends Component {
   render() {
 
     if(this.props.data){
-      var name = this.props.data.name;
-      var occupation= this.props.data.occupation;
-      var description= this.props.data.description;
-      var city= this.props.data.address.city;
+      var heading = this.props.data.heading;
+      var subheading= this.props.data.subheading;
       var networks= this.props.data.social.map(function(network){
         return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
       })
@@ -34,8 +32,8 @@ class Header extends Component {
 
       <div className="row banner">
          <div className="banner-text">
-            <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3>
+            <h1 className="responsive-headline">{heading}</h1>
+            <h3>{subheading}</h3>
             <hr />
             <ul className="social">
                {networks}
